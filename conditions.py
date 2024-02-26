107ac1883d1f0cd3d5d7081ccaadf50ab2d9c7cb
Sai <telugusoftwarehub@gmail.com>
2024-02-26 08:15:55 +0530

    Conditions[if, if else and elsif] and For loop an d While loop

---
+++ conditions.py
@@ -0,0 +1,6 @@
+# if True:
+#     print("This is True case")
+
+if True:
+    if True:
+        print("This is nested condition!")
---
+++ loops.py
@@ -0,0 +1,55 @@
+# my_list = [1,2,3,4,5]
+super_heros = ['Spider Man', 'Iron Man', 'Captian America']
+
+# loops
+# for(let i = 0; i< 10; i++)
+
+# for num in my_list:
+#     print(num)
+
+# for hero in super_heros:
+#     if hero == 'Iron Man':
+#         break
+#     print("Break: ", hero)
+
+# for hero in super_heros:
+#     if hero == 'Iron Man':
+#         continue
+#     print("Continue: ",hero)
+
+#function - for and else
+# for x in range(6):
+#     print(x)
+# else:
+#     print("Finally Finished!")
+   
+# for x in range(2,6):
+#     print(x)
+
+# for x in range(2, 20, 2):
+#     print(x)
+
+
+# fruits = ["apple", "orange", "kiwi", "banana"]
+# colours = ["red", "orange", "green", "yellow"]
+
+# for fruit in fruits:
+#     for colour in colours:
+#         print(fruit, colour)
+        
+# for fruit in fruits:
+#     pass
+
+#for, while
+
+i = 1
+
+while i < 20:
+    print(i)
+    i += 1
+    # if i == 10:
+        #break # to break loop
+        # continue
+else:
+    print("While loop was done!")
+    
